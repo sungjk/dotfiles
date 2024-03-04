36d4e7e53cd3791d844633538a4c4aaba1cd7a3d
Jeremy Kim <ajax0615@gmail.com>
2024-03-04 11:59:50 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ DOTFILES=$HOME/dotfiles
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Make ZSH the default shell environment
@@ -22,14 +22,13 @@ chsh -s $(which zsh)
 # Install zsh
 #-------------------------------------------------------------------------------
 ZSH=$HOME/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions $ZSH/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH/custom/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/jhipster/jhipster-oh-my-zsh-plugin.git $ZSH/custom/plugins/jhipster
-git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git $ZSH/custom/plugins/autoswitch_virtualenv
 
 brew install wget
 wget https://raw.githubusercontent.com/jeremyFreeAgent/oh-my-zsh-powerline-theme/master/powerline.zsh-theme -O $ZSH/themes/powerline.zsh-theme
